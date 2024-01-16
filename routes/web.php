5980227f4617df17b92b447de1f394b19c8113bb
Francis Wainaina Gitau <franciswainainag@gmail.com>
2024-01-16 20:56:28 +0300

    Api Routing and Blade Templates

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+        [    'id' => 1,
+            'title' => 'Listing One',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus metus ut nulla ultrices eleifend. Cras diam sapien, finibus eu dapibus vel, tincidunt quis tellus.'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Listing Two',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus metus ut nulla ultrices eleifend. Cras diam sapien, finibus eu dapibus vel, tincidunt quis tellus.'
+        ]
+    ]
+    ]);
 });

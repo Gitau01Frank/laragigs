c7378ae4722e9cfccb4fd1660f0704ef4f495232
Francis Wainaina Gitau <franciswainainag@gmail.com>
2024-01-17 17:54:51 +0300

    Seeding and Factories

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,26 +1,11 @@
 <?php
-    namespace App\Models;
 
-    class Listing {
-        public static function all() {
-           return  [
-            [     'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus metus ut nulla ultrices eleifend. Cras diam sapien, finibus eu dapibus vel, tincidunt quis tellus.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus metus ut nulla ultrices eleifend. Cras diam sapien, finibus eu dapibus vel, tincidunt quis tellus.'
-            ]
-            ];
-    }
-    public static function find($id) {
-        $listings = self::all();
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
 }
--- database/migrations/2024_01_17_085043_create_listings_table.php
+++ database/migrations/2024_01_17_085043_create_listings_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('tags');
-            $table->string('comapany');
+            $table->string('company');
             $table->string('location');
             $table->string('email');
             $table->string('website');

61f81afe8f9d8b0ad4b12776368a574f5b75f5d7
Francis Wainaina Gitau <franciswainainag@gmail.com>
2024-01-23 19:41:10 +0300

    File Uploading

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -7,7 +7,7 @@
                         <p class="mb-4">Post a gig to find a developer</p>
                     </header>
 
-                    <form method="POST" action="/listings">
+                    <form method="POST" action="/listings" enctype="multipart/form-data">
                         @csrf
                         <div class="mb-6">
                             <label
@@ -97,7 +97,7 @@
                             @enderror('tags')
                         </div>
 
-                       {{-- <div class="mb-6">
+                       <div class="mb-6">
                             <label for="logo" class="inline-block text-lg mb-2">
                                 Company Logo
                             </label>
@@ -106,7 +106,11 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="logo"
                             />
-                        </div> --}}
+
+                            @error('logo')
+                            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                        @enderror('tags')
+                        </div>
 
                         <div class="mb-6">
                             <label
@@ -119,8 +123,8 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="description"
                                 rows="10"
-                                placeholder="Include tasks, requirements, salary, etc">
-                                value="{{old('company')}}"
+                                placeholder="Include tasks, requirements, salary, etc"
+                                value="{{old('company')}}">
                             </textarea>
                         </div>
                         @error('description')

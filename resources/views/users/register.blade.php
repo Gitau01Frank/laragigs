24f03da5e3d6c2cc4d99c5ea50835f63ece66c1e
Francis Wainaina Gitau <franciswainainag@gmail.com>
2024-01-24 22:29:50 +0300

    User Login

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -16,7 +16,7 @@
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="name" value={{old('name')}}/>
+                name="name" value="{{old('name')}}"/>
 
             @error('name')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -31,7 +31,7 @@
             <input
                 type="email"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="email" value={{old('email')}}/>
+                name="email" value="{{old('email')}}"/>
             
             @error('email')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -49,7 +49,7 @@
             <input
                 type="password"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="password" value={{old('password')}}/>
+                name="password" value="{{old('password')}}"/>
 
             @error('password')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -67,7 +67,7 @@
             <input
                 type="password"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="password_confirmation" value={{old('password_confirmation')}}/>
+                name="password_confirmation" value="{{old('password_confirmation')}}"/>
 
             @error('password_confirmation')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
